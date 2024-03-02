6beb0241fe82a52aeb84f7ffc3f783b042787cd6
Luck Tevin <luck.tevin@student.moringaschool.com>
2024-03-02 18:15:22 +0300

    done

---
+++ lib/app.py
@@ -0,0 +1,11 @@
+# lib/app.py
+print("Hello world!")
+# lib/app.py
+print("Hello world!")
+print("Hello sun!")
+print("Hello sky!")
+# lib/app.py
+print("Hello world!", end=" ")
+print("Hello sun!", end="!! ")
+print("Hello sky!", end="!!!\n")
+print("Hello World! Pass this test, please.")
